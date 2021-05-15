dabb0e72cd207c7faa299092803b73a435fd54c9
Amine-Aziz <71028463+Amine-Aziz@users.noreply.github.com>
2021-05-15 23:24:48 +0000

    Add files via upload

---
+++ error302.php
@@ -0,0 +1,39 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=d jvice-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>302 Error</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:700,900" rel="stylesheet">
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="szBot/mr-robot.css">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+</head>
+<body>
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>302</h1>
+				<h2>Moved Permanently</h2>
+			</div>
+			<a href="https://Hard-Gamers.Com/">Homepage</a>
+		</div>
+	</div>
+</body><!-- 	Copyright © <a href="https://hard-gamers.com/" target="_blank">HardGamers Community</a> 2021 - All rights reserved -->
+
+</html>
